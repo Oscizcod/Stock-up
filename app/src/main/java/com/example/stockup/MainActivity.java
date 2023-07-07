2ce19323e744a6b1dcd6e201a793f70067d9e3f7
Pravin <pravin_muruhaya@hotmail.com>
2023-07-07 14:43:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/stockup/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.stockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
